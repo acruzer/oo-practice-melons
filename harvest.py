7bb7e12c35fb7ad555c24bc49bee6005c1a7d938
Hackbright Student <no-reply@hackbrightacademy.com>
2018-07-15 15:17:43 -0700

    finished part one

--- harvest.py
+++ harvest.py
@@ -62,7 +62,8 @@ def make_melon_types():
 
 	return all_melon_types
 
-#print(make_melon_types()[1].pairings)
+#print(make_melon_types())
+#print(make_melon_types()[0].code)
 
 def print_pairing_info(melon_types):
 	"""Prints information about each melon type's pairings."""
@@ -75,12 +76,22 @@ def print_pairing_info(melon_types):
 
 		print("{} pairs with\n - {}\n".format(melon.name,var_pairings))
 
-print_pairing_info(make_melon_types())
+#print_pairing_info(make_melon_types())
 
 def make_melon_type_lookup(melon_types):
 	"""Takes a list of MelonTypes and returns a dictionary of melon type by code."""
 
 	# Fill in the rest
+	melon_dict = {}
+	for instance in melon_types:
+		melon_dict[instance.code] = instance
+
+#	for key,value in melon_dict.items():
+#		print(key, value)
+	return melon_dict
+
+make_melon_type_lookup(make_melon_types())
+
 
 ############
 # Part 2   #
@@ -88,9 +99,30 @@ def make_melon_type_lookup(melon_types):
 
 class Melon(object):
 	"""A melon in a melon harvest."""
+	def __init__(self,melon_type,shape_rating,color_rating,field,harvester):
+		self.melon_type = melon_type
+		self.shape_rating = shape_rating
+		self.color_rating = color_rating
+		self.field = field
+		self.harvester = harvester
 
 	# Fill in the rest
 	# Needs __init__ and is_sellable methods
+	#melon_dict = make_melon_type_lookup(make_melon_types())
+
+
+	def is_sellable(self):
+
+		if self.field == 3:
+			return False
+		elif self.shape_rating < 5 and self.color_rating < 5:
+			return False
+		else: 
+			return True
+
+
+		
+
 
 def make_melons(melon_types):
 	"""Returns a list of Melon objects."""
@@ -104,3 +136,11 @@ def get_sellability_report(melons):
 
 
 
+melon1 = Melon("yw",8,7,2,"Shiela")
+
+print(melon1.is_sellable())
+
+
+melon_dict = make_melon_type_lookup(make_melon_types())
+
+print(melon_dict[melon1.melon_type].first_harvest)
